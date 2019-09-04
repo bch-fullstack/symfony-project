9beaeabd93906b2ec35830354856dac5202e1cb2
Hoang Pham <tienhoang.pham@bc.fi>
2019-09-04 12:50:44 +0300

    initla commit

---
+++ src/Controller/AdminController.php
@@ -0,0 +1,29 @@
+<?php 
+    namespace App\Controller;
+
+    use Symfony\Component\HttpFoundation\Response;
+    use Symfony\Component\Routing\Annotation\Route;
+    use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+    class AdminController extends AbstractController {
+        /**
+         * @Route("/", name="home")
+         */
+        public function index() {
+            $data = [];
+            $data['taskList'] = ['Make a dough', 'Spread toppings', 'Bake it up'];
+            return $this->render("/todo/list.html.twig", $data);
+        }
+
+        /**
+         * @Route("/clients", name="client_page")
+         */
+        public function clients() {
+            return new Response("
+            <html>
+                <body>
+                    <div>hello from admin controller clients</div>    
+                </body>
+            </html>");
+        }
+    }
---
+++ src/Controller/ClientController.php
@@ -0,0 +1,21 @@
+<?php 
+    namespace App\Controller;
+
+    use Symfony\Component\HttpFoundation\Response;
+    use Symfony\Component\Routing\Annotation\Route;
+    use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+    class ClientController extends AbstractController {
+        /**
+         * @Route("/subclient", name="client_controller")
+         */
+        public function index() {
+            return new Response("
+            <html>
+                <body>
+                    <div>hello from client controller</div>    
+                </body>
+            </html>");
+        }
+
+    }
