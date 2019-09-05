6b4c9a077f6574d528f743b3b4ec00af93acc309
Hoang Pham <tienhoang.pham@bc.fi>
2019-09-05 12:05:11 +0300

    Doctrine lecture, create new Tasks entity and write info to database

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -1,29 +1,38 @@
 <?php 
     namespace App\Controller;
 
+    use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
     use Symfony\Component\HttpFoundation\Response;
     use Symfony\Component\Routing\Annotation\Route;
-    use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+    use Symfony\Component\HttpFoundation\Request;
+    use App\Entity\Tasks;
 
     class AdminController extends AbstractController {
         /**
          * @Route("/", name="home")
+         * @return Response index.html.twig
          */
         public function index() {
             $data = [];
+            $data['user'] = 'Hoang';
             $data['taskList'] = ['Make a dough', 'Spread toppings', 'Bake it up'];
             return $this->render("/todo/list.html.twig", $data);
         }
 
         /**
-         * @Route("/clients", name="client_page")
+         * @Route("/task/save/{task_name}", name="create_new_task")
          */
-        public function clients() {
-            return new Response("
-            <html>
-                <body>
-                    <div>hello from admin controller clients</div>    
-                </body>
-            </html>");
+        public function clients(Request $request, $task_name) {
+            $entityManager = $this->getDoctrine()->getManager();
+
+            $tasks = new Tasks();
+            $tasks->setName($task_name);
+            $tasks->setDeadline(30);
+
+            $entityManager->persist($tasks);
+
+            $entityManager->flush();
+
+            return new Response("Hey! A new task has been saved with the id of: " . $tasks->getId());
         }
     }
